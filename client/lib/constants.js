132beda4962bd0cf6cf0248b73146af4a449e3d0
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2018-03-01 22:21:42 +0900

    init complete

---
+++ client/lib/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0xA1A05dD6a71500B3C47D437FDE9977b9cf3205E2'
---
+++ client/lib/sanityClient.js
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient({
+    projectId: 'er9nkx16',
+    dataset: 'production',
+    apiVersion: 'v1',
+    token: 'skzf4AEVOAqxL4Wm4dk2ZZbp109Su4lLDLQm9QCe1O3uemruxzt2CgRv4wjEybEJDMz5lmm2B74BP97Owfnc9VX2guIYLEAtdVvN9uyF7qXiSpJIiCyfA9qZ9nRsKYO9lpljJwMWsOILP2eCM3K0vbKltuE2OFA9Ui4x3OjkRxl3w6ooD8Jf',
+    useCdn: false,
+})
